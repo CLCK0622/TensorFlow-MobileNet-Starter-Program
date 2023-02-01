4c7cf7bfe60bf1b512720eb73c0ec3fce539779a
Kevin Zhong <zhongyi070622@163.com>
2023-02-01 23:35:56 +0800

    enhancement

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -1,13 +1,13 @@
 
 function upload(file) {
     if (file !== undefined)
-        document.getElementById("mainimg").src = URL.createObjectURL(file);
+        document.getElementById("mainImg").src = URL.createObjectURL(file);
     classify();
 }
 
 function replaceImg() {
     let imgAddr = document.getElementById("submitUrl").value;
-    document.getElementById("mainimg").src = imgAddr;
+    document.getElementById("mainImg").src = imgAddr;
     console.log(imgAddr);
 }
 
@@ -19,7 +19,7 @@ function displayImg() {
     imgEl.innerHTML += "<u>Examples</u>: ";
     for (let i = 0; i < 4; i++) {
         console.log("example" + (i + 1));
-        imgEl.innerHTML += '<img id="example" class="rounded-lg" alt="Picture Load Failed! " src="/static/img/imgrec/' + album[i] + '.jpg" style="width: 10%; margin: 10px; display: inline; " onclick="document.getElementById(' + "'mainimg'" + ').src = this.src;" />'
+        imgEl.innerHTML += '<img id="example" class="rounded-lg" alt="Picture Load Failed! " src="/static/img/imgrec/' + album[i] + '.jpg" style="width: 10%; margin: 10px; display: inline; " onclick="document.getElementById(' + "'mainImg'" + ').src = this.src;" />'
     }
     document.getElementById("exButt").innerHTML = "";
     // console.log(imgEl);
@@ -46,7 +46,7 @@ async function loadMobilenet() {
 
 async function classify() {
     await net;
-    const img = document.getElementById("mainimg");
+    const img = document.getElementById("mainImg");
     console.log(img);
     const ans = await net.classify(img);
     console.log(ans);
