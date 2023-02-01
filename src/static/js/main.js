6db40013c712d5ca1ebc0e24552cb03506d3bc85
Kevin Zhong <zhongyi070622@163.com>
2023-02-01 20:30:04 +0800

    upd examples

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -8,3 +8,17 @@ function replaceImg() {
     document.getElementById("mainimg").src = imgAddr;
     console.log(imgAddr);
 }
+
+let album = ['cat1', 'cat2', 'dog1', 'dog2'];
+
+function displayImg() {
+    console.log("render begin! ");
+    const imgEl = document.getElementById("exampleEl");
+    imgEl.innerHTML += "<u>Examples</u>: ";
+    for (let i = 0; i < 4; i++) {
+        console.log("example" + (i + 1));
+        imgEl.innerHTML += '<img id="img1" class="shadow-2xl bg-white rounded-lg" alt="Picture Load Failed! " src="/static/img/imgrec/' + album[i] + '.jpg"  width="10%" style="display: inline; margin: 10px; " />'
+    }
+    document.getElementById("exButt").innerHTML = "";
+    // console.log(imgEl);
+}
