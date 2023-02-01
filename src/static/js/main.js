c044a7592dd10d66c260bba221855d5b649ee9a1
Kevin Zhong <zhongyi070622@163.com>
2023-02-01 20:43:54 +0800

    Update main.js

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -17,7 +17,7 @@ function displayImg() {
     imgEl.innerHTML += "<u>Examples</u>: ";
     for (let i = 0; i < 4; i++) {
         console.log("example" + (i + 1));
-        imgEl.innerHTML += '<img id="img1" class="shadow-2xl bg-white rounded-lg" alt="Picture Load Failed! " src="/static/img/imgrec/' + album[i] + '.jpg"  width="10%" style="display: inline; margin: 10px; " />'
+        imgEl.innerHTML += '<img id="img1" class="shadow-2xl bg-white rounded-lg" alt="Picture Load Failed! " src="/static/img/imgrec/' + album[i] + '.jpg"  width="10%" style="display: inline; margin: 10px; " onclick="document.getElementById(' + "'mainimg'" + ').src = this.src;" />'
     }
     document.getElementById("exButt").innerHTML = "";
     // console.log(imgEl);
